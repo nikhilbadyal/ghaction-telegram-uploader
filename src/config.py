73893709cfc13ce04022457276aceb0b42254de4
Nikhil Badyal <nikhill773384@gmail.com>
2023-09-02 13:42:34 +0530

    🎨 Extract constant API url

--- src/config.py
+++ src/config.py
@@ -5,7 +5,7 @@ from typing import Self
 from environs import Env
 from requests import Session
 
-from src.constant import default_sticker
+from src.constant import GITHUB_API_LATEST_RELEASE_URL, default_sticker
 
 session = Session()
 
@@ -26,8 +26,8 @@ class UploaderConfig(object):
         self.bot_token = env.str("INPUT_BOT_TOKEN")
         self.GITHUB_REPOSITORY = env.str("INPUT_DOWNLOAD_GITHUB_REPOSITORY", env.str("GITHUB_REPOSITORY"))
         self.CHANGELOG_GITHUB_REPOSITORY = env.str("INPUT_CHANGELOG_GITHUB_REPOSITORY", self.GITHUB_REPOSITORY)
-        self.repo_url = f"https://api.github.com/repos/{self.GITHUB_REPOSITORY}/releases/latest"
-        self.changelog_url = f"https://api.github.com/repos/{self.CHANGELOG_GITHUB_REPOSITORY}/releases/latest"
+        self.repo_url = GITHUB_API_LATEST_RELEASE_URL.format(self.GITHUB_REPOSITORY)
+        self.changelog_url = GITHUB_API_LATEST_RELEASE_URL.format(self.CHANGELOG_GITHUB_REPOSITORY)
         self.personal_access_token = env.str("INPUT_PERSONAL_ACCESS_TOKEN", None)
         self.temp_folder_name = "apks"
         self.temp_folder = Path(self.temp_folder_name)
--- src/constant.py
+++ src/constant.py
@@ -5,3 +5,6 @@ REQUEST_TIMEOUT = 60
 default_sticker = "CAACAgUAAxkBAAEYpFpjOplSFK_q93KWoJKqWHGfgPMxMwACuAYAApqD2VV9UCzjLNawRCoE"
 temp_folder = Path(f"{Path.cwd()}/apks")
 status_code_200 = 200
+GITHUB_API_BASE_URL = "https://api.github.com"
+GITHUB_API_REPO_URL = GITHUB_API_BASE_URL + "/repos/{}"
+GITHUB_API_LATEST_RELEASE_URL = GITHUB_API_REPO_URL + "/releases/latest"
