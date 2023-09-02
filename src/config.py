74ea5b62b433b57458f2b687b6aba99eb1950019
Nikhil Badyal <nikhill773384@gmail.com>
2023-09-02 11:01:32 +0530

    🎨 Added config and linters

--- src/config.py
+++ src/config.py
@@ -1,7 +1,7 @@
 """Env Configurations."""
 from typing import Self
 
-from environs import Env, EnvValidationError
+from environs import Env
 from requests import Session
 
 from src.constant import default_sticker
@@ -15,18 +15,9 @@ class UploaderConfig(object):
     def __init__(self: Self, env: Env) -> None:
         self.env = env
         self.assets_pattern = env.str("INPUT_ASSETS_PATTERN", ".*")
-        try:
-            self.send_message = env.bool("INPUT_SEND_MESSAGE", True)
-        except EnvValidationError:
-            self.send_message = True
-        try:
-            self.send_sticker = env.bool("INPUT_SEND_STICKER", False)
-        except EnvValidationError:
-            self.send_sticker = False
-        try:
-            self.message = env.str("INPUT_MESSAGE", None)
-        except EnvValidationError:
-            self.message = None
+        self.send_message = env.bool("INPUT_SEND_MESSAGE", True)
+        self.send_sticker = env.bool("INPUT_SEND_STICKER", False)
+        self.message = env.str("INPUT_MESSAGE", None)
         self.sticker_id = env.str("INPUT_STICKER_ID", default_sticker)
         self.chat_id = env.int("INPUT_CHAT_ID")
         self.api_id = env.str("INPUT_API_ID")
